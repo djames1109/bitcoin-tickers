3eca4e7e10e979c9951df4337428617fa14e3fbe
DJames <davejames1109@gmail.com>
2021-02-28 01:50:40 +0800

    Initial Commit

---
+++ src/main/java/com/hachichu/bitcointickers/BitcoinTickersApplication.java
@@ -0,0 +1,13 @@
+package com.hachichu.bitcointickers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinTickersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitcoinTickersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hachichu/bitcointickers/BitcoinTickersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hachichu.bitcointickers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinTickersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
